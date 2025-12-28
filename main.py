d750cec17bf25d3e8e8ccfe8990dfed308e75b60
Abhinav65 <abinavijay63@gmail.com>
2025-12-29 00:00:42 +0530

    Add port-level packet details for TCP, UDP, and ICMP

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from scapy.all import *
 
-def proto(pkt):
-	if TCP in pkt: return "TCP"
-	elif UDP in pkt: return"UDP"
-	elif ICMP in pkt: return "ICMP"
-	else: return "Other"
-
-def print_packet_info(frames):
-	if IP not in frames:
+def print_packet_info(pkt):
+	if IP not in pkt:
 		return
-	print("SRC: " + frames[IP].src + " ->  DST: " + frames[IP].dst + " | " + proto(frames))
 
-frames = sniff(prn = print_packet_info)
+	if TCP in pkt:
+		print(f"SRC: {pkt[IP].src}:{pkt[TCP].sport} ->  DST: {pkt[IP].dst}:{pkt[TCP].dport} | TCP ")
+	elif UDP in pkt:
+		print(f"SRC: {pkt[IP].src}:{pkt[UDP].sport} ->  DST: {pkt[IP].dst}:{pkt[UDP].dport} | UDP ")
+	elif ICMP in pkt:
+		print(f"SRC: {pkt[IP].src} ->  DST: {pkt[IP].dst} | ICMP ")
+	else:
+		print(f"SRC: {pkt[IP].src} ->  DST: {pkt[IP].dst} | Other ")
+	
+
+frames = sniff(prn = print_packet_info, timeout=10)
